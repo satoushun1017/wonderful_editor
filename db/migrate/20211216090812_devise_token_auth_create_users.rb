83ea506dae9346472cb9aa7b2280567e58ca40ff
佐藤駿 <k4a3pgy4e@i.softbank.jp>
2021-12-17 18:11:26 +0900

    Revert "- CIで使うテスト用のDBの設定ファイルを追加"
    This reverts commit 82c36fafe0575180ae4bb1626c58fea491c286f8.

--- db/migrate/20211216090812_devise_token_auth_create_users.rb
+++ db/migrate/20211216090812_devise_token_auth_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
-
+    
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
@@ -30,7 +30,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
 
       ## User Info
       t.string :name
-      # t.string :nickname
+      t.string :nickname
       t.string :image
       t.string :email
 
